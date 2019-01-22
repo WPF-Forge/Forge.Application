5f8decc2f2e886da3713e975fdc94d6072ecfe53
Marcos Maia <mvmcjunior@gmail.com>
2019-01-22 17:15:55 -0200

    Ported projects to .net core 3.0

--- src/Forge.Application.Demo/App.xaml.cs
+++ src/Forge.Application.Demo/App.xaml.cs
@@ -8,14 +8,6 @@
     /// </summary>
     public partial class App
     {
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            var app = new App();
-            app.InitializeComponent();
-            app.Run();
-        }
-
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
