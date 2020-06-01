58ba417a2be489cdb0bd5313e96c48701b1d62f5
Marcos Cordeiro <marcos.cordeiro@cedrotech.com>
2020-06-01 16:16:31 -0300

    Update packages

--- src/Forge.Application/Infrastructure/Internal/PaletteService.cs
+++ src/Forge.Application/Infrastructure/Internal/PaletteService.cs
@@ -1,6 +1,26 @@
-﻿namespace Forge.Application.Infrastructure.Internal
+﻿using System.Windows.Media;
+
+namespace Forge.Application.Infrastructure.Internal
 {
     using MaterialDesignThemes.Wpf;
+
+    internal static class ThemeExtensions
+    {
+        public static void SetPrimaryColor(this ITheme theme, string primaryColor)
+        {
+            theme.SetPrimaryColor(ToColor(primaryColor));
+        }
+
+        public static void SetSecondaryColor(this ITheme theme, string accentColor)
+        {
+            theme.SetSecondaryColor(ToColor(accentColor));
+        }
+
+        private static Color ToColor(string hex)
+        {
+            return (Color) ColorConverter.ConvertFromString(hex);
+        }
+    }
 
     internal class PaletteService : IPaletteService
     {
@@ -16,34 +36,39 @@
 
         public void RefreshTheme()
         {
-            new PaletteHelper().SetLightDark(this.DarkMode);
+            var paletteHelper = new PaletteHelper();
+            var theme = paletteHelper.GetTheme();
+            theme.SetBaseTheme(this.DarkMode ? Theme.Dark : Theme.Light);
+            paletteHelper.SetTheme(theme);
         }
+
 
         public void RefreshPalette()
         {
             var paletteHelper = new PaletteHelper();
+            var theme = paletteHelper.GetTheme();
             if (this.DarkMode)
             {
                 if (this.DarkModePrimary != null)
                 {
-                    paletteHelper.ReplacePrimaryColor(this.DarkModePrimary);
+                    theme.SetPrimaryColor(this.DarkModePrimary);
                 }
 
                 if (this.DarkModeAccent != null)
                 {
-                    paletteHelper.ReplaceAccentColor(this.DarkModeAccent);
+                    theme.SetSecondaryColor(this.DarkModeAccent);
                 }
             }
             else
             {
                 if (this.LightModePrimary != null)
                 {
-                    paletteHelper.ReplacePrimaryColor(this.LightModePrimary);
+                    theme.SetPrimaryColor(this.LightModePrimary);
                 }
 
                 if (this.LightModeAccent != null)
                 {
-                    paletteHelper.ReplaceAccentColor(this.LightModeAccent);
+                    theme.SetSecondaryColor(this.LightModeAccent);
                 }
             }
         }
